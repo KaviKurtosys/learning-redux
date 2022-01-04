7b0883e212616ebbfb558b983c74c7a91485e5a9
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-01-04 16:14:50 +0200

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -4,42 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-
-// STORE -> GLOBALIZED STATE
-
-// ACTION
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-}
-
-let store = createStore(counter);
-
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH
-store.dispatch(increment());
-
-
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <React.StrictMode>
